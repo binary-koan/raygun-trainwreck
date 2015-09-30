c8891f5165c794035f7267281a78a47a7a1c3efb
Jono Mingard <reason.koan@gmail.com>
2015-09-30 02:50:06 +0000

    Add validations to wreck model

--- app/models/wreck.rb
+++ app/models/wreck.rb
@@ -4,4 +4,15 @@ class Wreck
   field :is_epic, type: Mongoid::Boolean
   field :is_boring, type: Mongoid::Boolean
   field :is_spectacular, type: Mongoid::Boolean
+
+  # Random validations for testing
+
+  validates_uniqueness_of :message
+  validates_length_of :message, minimum: 10, maximum: 100
+
+  before_validation do |doc|
+    if doc.is_boring && (doc.is_epic || doc.is_spectacular)
+      doc.errors.add :is_boring, "An error can't be epic or spectacular if it's boring!"
+    end
+  end
 end
